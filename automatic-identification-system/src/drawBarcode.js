33b7c0d52419e9d9c8c4d55c9166c654dc03d32c
Danylo <danylod@wix.com>
2024-12-12 11:04:59 +0200

    automatic-identification-system: added barcode paddings

--- automatic-identification-system/src/drawBarcode.js
+++ automatic-identification-system/src/drawBarcode.js
@@ -1,3 +1,5 @@
+const PADDING = 5;
+
 export const draw2of5Barcode = (barcode, size) => {
   const narrowWidth = 3 * size;
   const lineHeight = 100 * size;
@@ -5,14 +7,14 @@ export const draw2of5Barcode = (barcode, size) => {
   const canvas = document.createElement('canvas');
   const ctx = canvas.getContext("2d");
 
-  let xPosition = 0;
+  let xPosition = 5;
 
   const N_COUNT = barcode.match(/N/g).length;
   const W_COUNT = barcode.match(/W/g).length;
   const S_COUNT = barcode.match(/S/g).length;
 
-  canvas.width = N_COUNT * narrowWidth + W_COUNT * wideLineWidth + S_COUNT * narrowWidth;
-  canvas.height = lineHeight;
+  canvas.width = N_COUNT * narrowWidth + W_COUNT * wideLineWidth + S_COUNT * narrowWidth + 2 * PADDING;
+  canvas.height = lineHeight + 2 * PADDING;
 
   ctx.fillStyle = 'white'; // Or any desired color
   ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -26,7 +28,7 @@ export const draw2of5Barcode = (barcode, size) => {
 
       if (isBlack) {
         ctx.fillStyle = "black";
-        ctx.fillRect(xPosition, 0, lineWidth, lineHeight);
+        ctx.fillRect(xPosition, PADDING, lineWidth, lineHeight);
       }
 
       xPosition += lineWidth;
